b01171a449d29b6da7972fcbea252e9a719f241d
Michał Świderski <swiderskimichal@outlook.com>
2020-11-21 23:46:09 +0100

    optimizations

--- zad3/cbc/cbc.py
+++ zad3/cbc/cbc.py
@@ -6,29 +6,26 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
 
+
+def xor(a, b):
+    return (int.from_bytes(a, 'big') ^ int.from_bytes(b, 'big')).to_bytes(max(len(a), len(b)), 'big')
+
+
 KEY = get_random_bytes(16)
 IV = get_random_bytes(16)
 
 input_file = open(argv[1], 'rb')
-content = pad(input_file.read(), 16)
-input_file.close()
-
-blocks = [content[i:i + 16] for i in range(0, len(content), 16)]
-
-ciphertext = bytes()
+output = open(argv[2], 'wb')
 block_ciphertext = None
-for block in blocks:
-    if block_ciphertext is None:
-        xored = (int.from_bytes(block, 'big') ^ int.from_bytes(IV, 'big')).to_bytes(len(block), 'big')
-    else:
-        xored = (int.from_bytes(block, 'big') ^ int.from_bytes(block_ciphertext, 'big')).to_bytes(len(block), 'big')
-    cipher = AES.new(KEY, AES.MODE_ECB)
+cipher = AES.new(KEY, AES.MODE_ECB)
+while block := input_file.read(16):
+    if len(block) < 16:
+        block = pad(block, 16)
+    xored = xor(block, block_ciphertext) if block_ciphertext else xor(block, IV)
     block_ciphertext = cipher.encrypt(xored)
-    ciphertext += block_ciphertext
+    output.write(block_ciphertext)
 
+output.close()
 print(f'KEY={KEY.hex()}')
 print(f'IV={IV.hex()}')
 print(f'openssl enc -d -aes-128-cbc -iv {IV.hex()} -K {KEY.hex()} -nosalt -in {argv[2]} -out decrypted')
-output = open(argv[2], 'wb')
-output.write(ciphertext)
-output.close()
